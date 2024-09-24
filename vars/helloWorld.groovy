8a9bcea91448f13a453071e288c16aed96e9cf19
herb.huang <herb.huang@neutec.com.tw>
2024-09-24 22:32:11 +0800

    revise

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,13 @@
+import com.serverInfo.dto.ServerInfo
+
 def call() {
+
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
     echo "Hello World! My first Shared Library"
-    new com.serverInfo.dao.ServerInfoDAO().findServerInfo('SELECT * FROM serverinfo')
+    ServerInfoDAO serverInfoDAO = new com.serverInfo.dao.ServerInfoDAO()
+    List<ServerInfo> serverInfoList = serverInfoDAO.findServerInfo('SELECT * FROM serverinfo')
 }
