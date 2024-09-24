1d6caa17b3c9e1324deba5160ecb784880afe5d1
herb.huang <herb.huang@neutec.com.tw>
2024-09-24 22:26:08 +0800

    revise method

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
-import com.serverInfo.dao.ServerInfoDAO
+import com.serverInfo.dao
 
 def call() {
     echo "Hello World! My first Shared Library"
-    new ServerInfoDAO().findServerInfo('SELECT * FROM serverinfo')
+    new com.serverInfo.dao.ServerInfoDAO().findServerInfo('SELECT * FROM serverinfo')
 }
