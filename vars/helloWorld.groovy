236020525e2e0580bd32cae84a6953f7bcd98c0b
herb.huang <herb.huang@neutec.com.tw>
2024-09-25 14:31:53 +0800

    test 999

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 import groovy.sql.Sql
 import com.serverInfo.dto.ServerInfo
-import com.serverInfo.dao.ServerInfoDAO
 
 def test1() {
     println("Hello World! test1 ")
@@ -9,6 +8,40 @@ def test1() {
 
 def test2() {
     println("Hello World! test2 ")
-    ServerInfoDAO serverInfoDAO = new ServerInfoDAO(this)
-    List<ServerInfo> serverInfoList = serverInfoDAO.findServerInfo("SELECT * FROM serverinfo")
+//    ServerInfoDAO serverInfoDAO = new ServerInfoDAO(this)
+    List<ServerInfo> serverInfoList = findServerInfo("SELECT * FROM serverinfo")
+    println(serverInfoList.size())
+}
+
+def findServerInfo(String sql) {
+
+//        def dbPath = 'C:/Users/SAHerbHuangT14/initial-db.sqlite'
+    def dbPath = '/var/jenkins_home/sqlite_data/initial-db.sqlite'
+    println(dbPath)
+    def url = "jdbc:sqlite:$dbPath"
+    println(url)
+    def sqlInstance = Sql.newInstance(url, "org.sqlite.JDBC")
+    println(sqlInstance)
+    List<ServerInfo> serverInfoList = new ArrayList<>();
+    println(serverInfoList)
+    sqlInstance.eachRow(sql) { row ->
+        ServerInfo serverInfo = new ServerInfo()
+
+        serverInfo.setId(row.id)
+        serverInfo.setServerType(row.servertype)
+        serverInfo.setName(row.name)
+        serverInfo.setIp(row.ip)
+        serverInfo.setPort(row.port)
+        serverInfo.setStatus(row.status)
+        serverInfo.setUpdateDate(row.updatedate)
+
+        serverInfoList.add(serverInfo)
+
+        println serverInfo.toString()
+    }
+
+    sqlInstance.close()
+    println("===================")
+    println(serverInfoList)
+    return serverInfoList
 }
