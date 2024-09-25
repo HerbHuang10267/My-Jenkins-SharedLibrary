91fb0b8ea04361b8199cf23b3ffecbbc4a7fc806
herb.huang <herb.huang@neutec.com.tw>
2024-09-25 14:16:38 +0800

    test 666

--- src/com/serverInfo/dao/ServerInfoDAO.groovy
+++ src/com/serverInfo/dao/ServerInfoDAO.groovy
@@ -11,37 +11,38 @@ class ServerInfoDAO implements Serializable {
     def script
 
     ServerInfoDAO(script) {
-        this.script=script
+        this.script = script
     }
 
     def findServerInfo(String sql) {
-        script.println("do findServerInfo")
+
 //        def dbPath = 'C:/Users/SAHerbHuangT14/initial-db.sqlite'
-//        // def dbPath = '/var/jenkins_home/sqlite_data/initial-db.sqlite'
-//        def url = "jdbc:sqlite:$dbPath"
-//        def sqlInstance = Sql.newInstance(url, "org.sqlite.JDBC")
-//
-//        List<ServerInfo> serverInfoList = new ArrayList<>();
-//        sqlInstance.eachRow(sql) { row ->
-//            ServerInfo serverInfo = new ServerInfo()
-//
-//            serverInfo.setId(row.id)
-//            serverInfo.setServerType(row.servertype)
-//            serverInfo.setName(row.name)
-//            serverInfo.setIp(row.ip)
-//            serverInfo.setPort(row.port)
-//            serverInfo.setStatus(row.status)
-//            serverInfo.setUpdateDate(row.updatedate)
-//
-//            serverInfoList.add(serverInfo)
-//
-//            println serverInfo.toString()
-//        }
-//
-//        sqlInstance.close()
-//        return serverInfoList
-        script.println(sql)
-        return null
+        def dbPath = '/var/jenkins_home/sqlite_data/initial-db.sqlite'
+        script.println(dbPath)
+        def url = "jdbc:sqlite:$dbPath"
+        def sqlInstance = Sql.newInstance(url, "org.sqlite.JDBC")
+
+        List<ServerInfo> serverInfoList = new ArrayList<>();
+        sqlInstance.eachRow(sql) { row ->
+            ServerInfo serverInfo = new ServerInfo()
+
+            serverInfo.setId(row.id)
+            serverInfo.setServerType(row.servertype)
+            serverInfo.setName(row.name)
+            serverInfo.setIp(row.ip)
+            serverInfo.setPort(row.port)
+            serverInfo.setStatus(row.status)
+            serverInfo.setUpdateDate(row.updatedate)
+
+            serverInfoList.add(serverInfo)
+
+            script.println serverInfo.toString()
+        }
+
+        sqlInstance.close()
+        script.println("===================")
+        script.println(serverInfoList)
+        return serverInfoList
     }
 
 }
