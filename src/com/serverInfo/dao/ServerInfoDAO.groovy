ffb3af70be9801dea45f2785c301abdcceda0776
herb.huang <herb.huang@neutec.com.tw>
2024-09-25 12:10:41 +0800

    test

--- src/com/serverInfo/dao/ServerInfoDAO.groovy
+++ src/com/serverInfo/dao/ServerInfoDAO.groovy
@@ -8,32 +8,33 @@ import com.serverInfo.dto.ServerInfo
 
 class ServerInfoDAO {
 
-    static List<ServerInfo> findServerInfo(String sql) {
-        // def dbPath = 'C:/Users/SAHerbHuangT14/initial-db.sqlite'
-        def dbPath = '/var/jenkins_home/sqlite_data/initial-db.sqlite'
     def findServerInfo(String sql) {
-        def url = "jdbc:sqlite:$dbPath"
-        def sqlInstance = Sql.newInstance(url, "org.sqlite.JDBC")
-
-        List<ServerInfo> serverInfoList = new ArrayList<>();
-        sqlInstance.eachRow(sql) { row ->
-            ServerInfo serverInfo = new ServerInfo()
-
-            serverInfo.setId(row.id)
-            serverInfo.setServerType(row.servertype)
-            serverInfo.setName(row.name)
-            serverInfo.setIp(row.ip)
-            serverInfo.setPort(row.port)
-            serverInfo.setStatus(row.status)
-            serverInfo.setUpdateDate(row.updatedate)
-
-            serverInfoList.add(serverInfo)
-
-            println serverInfo.toString()
-        }
-
-        sqlInstance.close()
-        return serverInfoList
+//        def dbPath = 'C:/Users/SAHerbHuangT14/initial-db.sqlite'
+//        // def dbPath = '/var/jenkins_home/sqlite_data/initial-db.sqlite'
+//        def url = "jdbc:sqlite:$dbPath"
+//        def sqlInstance = Sql.newInstance(url, "org.sqlite.JDBC")
+//
+//        List<ServerInfo> serverInfoList = new ArrayList<>();
+//        sqlInstance.eachRow(sql) { row ->
+//            ServerInfo serverInfo = new ServerInfo()
+//
+//            serverInfo.setId(row.id)
+//            serverInfo.setServerType(row.servertype)
+//            serverInfo.setName(row.name)
+//            serverInfo.setIp(row.ip)
+//            serverInfo.setPort(row.port)
+//            serverInfo.setStatus(row.status)
+//            serverInfo.setUpdateDate(row.updatedate)
+//
+//            serverInfoList.add(serverInfo)
+//
+//            println serverInfo.toString()
+//        }
+//
+//        sqlInstance.close()
+//        return serverInfoList
+        println(sql)
+        return null
     }
 
 }
