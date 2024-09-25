dd60fc7f668c3f6a0c5db14cdef87ca2f20c682a
herb.huang <herb.huang@neutec.com.tw>
2024-09-25 17:15:26 +0800

    revise dto

--- src/com/serverInfo/dto/ServerInfo.groovy
+++ src/com/serverInfo/dto/ServerInfo.groovy
@@ -2,69 +2,13 @@ package com.serverInfo.dto
 
 class ServerInfo {
 
-    private int id;
-    private int serverType;
-    private String name;
-    private String port;
-    private String ip;
-    private int status;
-    private String updateDate;
-
-    int getId() {
-        return id
-    }
-
-    void setId(int id) {
-        this.id = id
-    }
-
-    int getServerType() {
-        return serverType
-    }
-
-    void setServerType(int serverType) {
-        this.serverType = serverType
-    }
-
-    String getName() {
-        return name
-    }
-
-    void setName(String name) {
-        this.name = name
-    }
-
-    String getPort() {
-        return port
-    }
-
-    void setPort(String port) {
-        this.port = port
-    }
-
-    String getIp() {
-        return ip
-    }
-
-    void setIp(String ip) {
-        this.ip = ip
-    }
-
-    int getStatus() {
-        return status
-    }
-
-    void setStatus(int status) {
-        this.status = status
-    }
-
-    String getUpdateDate() {
-        return updateDate
-    }
-
-    void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate
-    }
+    int id;
+    int serverType;
+    String name;
+    String port;
+    String ip;
+    int status;
+    String updateDate;
 
     @Override
     String toString() {
