d6e79705ef312ff7851a3e34ed6c383c6611801a
herb.huang <herb.huang@neutec.com.tw>
2024-09-26 10:21:20 +0800

    test

---
+++ vars/collectServerInfoByOption.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def call(String optionStr) {
+    Map serverInfoMap = [:]
+    String[] optionArray = optionStr.split(",")
+    for (int i = 0; i < optionArray.size(); i += 2) {
+        def key = optionArray[i]
+        def value = optionArray[i + 1].toBoolean()
+        serverInfoMap[key] = value
+    }
+    return serverInfoMap
+}
