5ddbbe8b8ba9275677357dc97c082eaf8155ca43
herb.huang <herb.huang@neutec.com.tw>
2024-09-26 12:20:20 +0800

    test

--- src/com/serverInfo/model/ServerInfoBO.groovy
+++ src/com/serverInfo/model/ServerInfoBO.groovy
@@ -2,4 +2,9 @@ package com.serverInfo.model
 
 class ServerInfoBO {
 
+    static def helloServerInfoBO() {
+        println("Hello, World! helloServerInfoBO")
+        return "Hello, World! helloServerInfoBO"
+    }
+
 }
--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
+import com.serverInfo.bo.ServerInfoBO
+
 def test1() {
     println("Hello World! test1 ")
     return "Hello World! test1"
@@ -7,5 +9,11 @@ def test1() {
 def callOtherMethod(Map config = [:]) {
     println("Hello World! test2 ")
     def result = serverInfoService.executeSQL(config)
+    return result
+}
+
+def callSrcServerInfoBO() {
+    println("Hello World! test3 ")
+    def result = ServerInfoBO.helloServerInfoBO()
     return result
 }
