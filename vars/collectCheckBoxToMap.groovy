b97f6a63e1ae221ca0dac073a9353ab33b133481
herb.huang <herb.huang@neutec.com.tw>
2024-09-26 12:00:51 +0800

    revise name

--- vars/collectCheckBoxToMap.groovy
+++ vars/collectCheckBoxToMap.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 def call(String optionStr) {
-    Map serverInfoMap = [:]
+    Map checkBoxMap = [:]
     String[] optionArray = optionStr.split(",")
     for (int i = 0; i < optionArray.size(); i += 2) {
         def key = optionArray[i]
         def value = optionArray[i + 1].toBoolean() ? 1 : 0
-        serverInfoMap[key] = value
+        checkBoxMap[key] = value
     }
-    return serverInfoMap
+    return checkBoxMap
 }
