757e749c427a4b9f66e1e7f7558cf0f47abd6b99
herb.huang <herb.huang@neutec.com.tw>
2024-10-02 14:33:58 +0800

    revise path

---
+++ src/com/serverInfo/service/ServerInfoService.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+package com.serverInfo.service
+
+import com.serverInfo.dto.ServerInfo
+import com.serverInfo.model.ServerInfoBO
+
+static void main(String[] args) {
+    this.println "START"
+    List<ServerInfo> serverInfoList = queryServerInfoList(project: "local")
+    println "==============="
+    for (ServerInfo serverInfo : serverInfoList) {
+        println serverInfo.toString()
+    }
+}
+
+def queryServerInfoList(Map config = [:]) {
+    return new ServerInfoBO().queryServerInfoList(config)
+}
+
+def staticupdateServerInfoStatus(Map config = [:], Map serverInfoMap = [:]) {
+    return new ServerInfoBO().updateServerInfoStatus(config, serverInfoMap)
+}
+
+def genServerStatusOptionHtml(Map config = [:]) {
+    return new ServerInfoBO().genServerStatusOptionHtml(config)
+}
+
+def queryActiveHostName(Map config = [:]) {
+    return new ServerInfoBO().queryActiveHostName(config)
+}
+
+return this
