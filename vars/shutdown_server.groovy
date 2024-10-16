8256902ad3a571bbb09f598336f894389fd982fd
herb.huang <herb.huang@neutec.com.tw>
2024-10-16 10:02:45 +0800

    test shut down

--- vars/shutdown_server.groovy
+++ vars/shutdown_server.groovy
@@ -1,6 +1,25 @@
 #!/usr/bin/env groovy
 import com.serverInfo.dto.ServerInfo
 
-def call(List<ServerInfo> serverInfoList) {
-    log.logServerInfo(serverInfoList)
+def call(ServerInfo serverInfo) {
+    log.logServerInfo(serverInfo)
+
+    def remoteUser = "root@${serverInfo.ip}"
+//    def remoteScript = serverInfo.shutDownShellPath
+    def remoteScript = "pwd"
+
+    if (remoteUser == null || remoteUser == "" ||
+        remoteScript == null || remoteScript == "") {
+        throw new Exception("remoteUser or remoteScript is null or empty")
+    }
+
+    // def command = ["ssh", remoteUser, "sh", remoteScript]
+    def command = ["ssh", remoteUser, remoteScript]
+
+    def proc = command.execute()
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitForOrKill(60 * 1000)
+    println("sout: ${sout}")
+    println("sout: ${serr}")
 }
