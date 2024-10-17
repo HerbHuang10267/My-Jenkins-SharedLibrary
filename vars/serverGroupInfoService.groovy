0ab98f06bb9426bd423b7f873432890585c6b7a4
herb.huang <herb.huang@neutec.com.tw>
2024-10-17 10:16:21 +0800

    revise

--- vars/serverGroupInfoService.groovy
+++ vars/serverGroupInfoService.groovy
@@ -5,33 +5,6 @@ import groovy.transform.Field
 
 @Field ServerGroupInfoBO serverGroupInfoBO = new ServerGroupInfoBO()
 
-static void main(String[] args) {
-    List<ServerGroupInfo> serverGroupInfoList = queryServerGroupInfoList(project: "local")
-    println "==============="
-    for (ServerGroupInfo serverGroupInfo : serverGroupInfoList) {
-        println serverGroupInfo.toString()
-    }
-
-    List startUpOrderList = serverGroupInfoList.sort { it.startUpOrder }.collect()
-    println startUpOrderList
-
-    List shutDownOrderList = serverGroupInfoList.sort { it.shutDownOrder }.collect()
-    println shutDownOrderList
-    println startUpOrderList
-}
-
-List serverGroupInfoList = queryServerGroupInfoList(project: "local")
-println "==============="
-for (ServerGroupInfo serverGroupInfo : serverGroupInfoList) {
-    println serverGroupInfo.toString()
-}
-
-List<ServerGroupInfo> shutDownServerOrderList = queryServerGroupInfoList(project: "local", sort: "shutdownorder")
-println "shutDownServerOrderList:" + shutDownServerOrderList
-
-List<ServerGroupInfo> startUpServerOrderList = queryServerGroupInfoList(project: "local", sort: "startuporder")
-println "startUpServerOrderList:" + startUpServerOrderList
-
 def queryServerGroupInfoList(Map config = [:]) {
     return serverGroupInfoBO.queryServerGroupInfoList(config)
 }
--- vars/serverInfoService.groovy
+++ vars/serverInfoService.groovy
@@ -1,18 +1,8 @@
 #!/usr/bin/env groovy
-import com.serverInfo.dto.ServerInfo
 import com.serverInfo.model.ServerInfoBO
 import groovy.transform.Field
 
 @Field ServerInfoBO serverInfoBO = new ServerInfoBO()
-
-static void main(String[] args) {
-    println "START"
-    List<ServerInfo> serverInfoList = queryServerInfoList(project: "local")
-    println "==============="
-    for (ServerInfo serverInfo : serverInfoList) {
-        println serverInfo.toString()
-    }
-}
 
 def queryServerInfoList(Map config = [:]) {
     return serverInfoBO.queryServerInfoList(config)
