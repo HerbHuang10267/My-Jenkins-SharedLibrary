f28e19af5fc555cb8a863a4806fee6c07ab898cc
herb.huang <herb.huang@neutec.com.tw>
2024-10-18 09:33:06 +0800

    test

--- vars/scp_war.groovy
+++ vars/scp_war.groovy
@@ -3,17 +3,20 @@ import com.serverInfo.dto.ServerInfo
 
 def call(ServerInfo serverInfo) {
 
-    def remoteScript = "root@${serverInfo.ip}:${serverInfo.tomcatPath}/ROOT.zip"
-    def command = ["scp", "target/ROOT.war", remoteScript]
-
-    def proc = command.execute()
-    def sout = new StringBuilder(), serr = new StringBuilder()
-    proc.consumeProcessOutput(sout, serr)
-    proc.waitForOrKill(60 * 1000)
-    println("exitValue: ${proc.exitValue()}")
-    if (proc.exitValue() == 0) {
-        println("sout: ${sout}")
-    } else {
-        error("error: ${serr}")
-    }
+    sh '''
+        echo "Hello scp war ${serverInfo.ip}"
+        pwd
+    '''
+//    def remoteScript = "root@${serverInfo.ip}:${serverInfo.tomcatPath}/ROOT.zip"
+//    def command = ["scp", "target/ROOT.war", remoteScript]
+//
+//    def proc = command.execute()
+//    def sout = new StringBuilder(), serr = new StringBuilder()
+//    proc.consumeProcessOutput(sout, serr)
+//    proc.waitForOrKill(60 * 1000)
+//    if (proc.exitValue() == 0) {
+//        println("sout: ${sout}")
+//    } else {
+//        error("error: ${serr}")
+//    }
 }
