3441f958d4deb799cbf0e70d11770fa04b0343e4
herb.huang <herb.huang@neutec.com.tw>
2024-10-22 13:41:05 +0800

    copy s3

---
+++ vars/aws_copy_from_s3.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+def call(Map config = [:]) {
+    def awsBucketName = config.awsBucketName
+    def awsProfile = config.awsProfile
+    sh """
+        mkdir -p target
+        rm -f ./target/ROOT.war
+        rm -rf ./target/ROOT
+
+        aws s3 cp s3://$awsBucketName/ROOT.war ./target --profile $awsProfile
+        unzip -oq ./target/ROOT.war -d ./target/ROOT
+        ls -al ./target
+    """
+}
