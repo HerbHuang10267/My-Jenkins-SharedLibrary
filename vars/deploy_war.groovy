85a07eae7e5cd850f6c7a9f256a405f3c419ebdd
herb.huang <herb.huang@neutec.com.tw>
2024-10-23 09:37:15 +0800

    check war

--- vars/deploy_war.groovy
+++ vars/deploy_war.groovy
@@ -4,5 +4,7 @@ import com.serverInfo.dto.ServerInfo
 def call(ServerInfo serverInfo) {
     sh """
         ssh -p56123 root@$serverInfo.ip sh $serverInfo.deployShellPath
+        
+        scp -p56123 root@$serverInfo.ip ls -alh $serverInfo.tomcatPath
     """
 }
