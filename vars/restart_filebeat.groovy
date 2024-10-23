9b44e01aaacb373e4a2e7e201f898265261f6b44
herb.huang <herb.huang@neutec.com.tw>
2024-10-23 11:17:37 +0800

    check log

--- vars/restart_filebeat.groovy
+++ vars/restart_filebeat.groovy
@@ -4,5 +4,7 @@ import com.serverInfo.dto.ServerInfo
 def call(ServerInfo serverInfo) {
     sh """
         ssh -p56123 root@$serverInfo.ip sh $serverInfo.filebeatShellPath
+
+        ssh -p56123 root@$serverInfo.ip ps -ef | grep filebeat
     """
 }
