fa82865b1b5280cd1eb5cfa0aa16affca7a7fdb2
herb.huang <herb.huang@neutec.com.tw>
2024-10-23 11:27:43 +0800

    remove check

--- vars/shutdown_maintain_server.groovy
+++ vars/shutdown_maintain_server.groovy
@@ -3,8 +3,8 @@ import com.serverInfo.dto.ServerInfo
 
 def call(ServerInfo serverInfo) {
     sh """
+
         ssh -p56123 root@$serverInfo.ip sh $serverInfo.shutDownMaintainShellPath
 
-        ssh -p56123 root@$serverInfo.ip ps -ef | grep tomcat
     """
 }
